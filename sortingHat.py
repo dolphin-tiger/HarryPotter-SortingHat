7db13ab9d4162e3ca4b499e2475ec0abb701cd5b
Jeremy Reynolds <JReynolds@teamlinx.com>
2020-03-28 18:35:09 -0600

    updated to remove additional unused code and comment sections for clarity.

--- sortingHat.py
+++ sortingHat.py
@@ -1,8 +1,6 @@
-# Simple test for NeoPixels on Raspberry Pi
 import time
 import board
 import neopixel
-#import digitalio
 import RPi.GPIO as GPIO
 import random
 
@@ -16,8 +14,11 @@ num_pixels = 12
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
 ORDER = neopixel.GRB
+
+# define the off color
 pixelOff = (0,0,0)
 
+#create the pixels object
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.7, auto_write=False, pixel_order=ORDER
 )
@@ -27,9 +28,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 btn = GPIO.input(21)
 
-## btn = digitalio.DigitalInOut(board.D21)
-## btn.direction = digitalio.Direction.INPUT
-## btn.pull = digitalio.Pull.UP
+#define the button press event.
 def btnPress(pin):
     for x in range(3):
         rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
@@ -42,7 +41,7 @@ def btnPress(pin):
 GPIO.add_event_detect(21, GPIO.RISING, callback=btnPress)
 
 
-#Colors
+#House Colors
 #Griffindor
 Scarlet  = (128,  0,  0)
 Gold     = ( 64, 64,  8)
@@ -101,13 +100,6 @@ def wheel(pos):
         b = int(255 - pos * 3)
     return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
 
-#def color_chase(color, wait):
-#    for i in range(num_pixels):
-#        pixels[i] = color
-#        time.sleep(wait)
-#        pixels.show()
-#    time.sleep(0.5)
-
 def colorTwo_chase(color1, color2, loops, wait):
     for l in range(loops):
         for i in range(num_pixels):
@@ -143,11 +135,9 @@ def rainbow_cycle(wait):
         time.sleep(wait)
 
 def displayHouse(c1, c2):
-    #pixels.fill(pixelOff)
-    #pixels.show()
-
     time.sleep(0.5)
     colorTwo_chase(c1, c2, 5, 0.1)
+    #comment out one of the following lines.  You can use either chase or alternating pixels
     #colorTwo_alternating(c1, c2, 25, 0.5)
     colorTwo_chase(c1, c1, 1, 0.1)
 
@@ -166,18 +156,4 @@ while True:
     if GPIO.event_detected(21):
         print('Hmm... but where to put you?')
     time.sleep(0.1)
-
-# this captures any button actions
-#while True:
-#    if btn.value == True and iB == 0:
-#        iB = 1
-#        for x in range(6):
-#            rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
-#            h = random.randint(0,3)
-#            houseName, c1, c2 = setHouse(h)
-#            print(houseName)
-#        displayHouse(c1, c2)
-#
-#    elif btn.value == False and iB == 1:
-#        iB = 0
-#    time.sleep(0.1)
+    
